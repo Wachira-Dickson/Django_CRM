b89baadc81ada6fe95abd1a87be698e687aa50da
Wachira-Dickson <dicksonwachira886@gmail.com>
2026-06-21 23:59:10 +0300

    Registration

--- website/views.py
+++ website/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    #Check to see if it is loggin in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have successfully logged in.')
+            return render(request, 'home.html', {})
+        else:
+            messages.error(request, 'Invalid username or password. Please try again.')
+    return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out...')
+    return redirect('home')
+
+def register_user(request):
+    return render(request, 'register.html', {})
+    register(request)
