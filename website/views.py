e08434bffc25b5a4700375dbb6aa1a12b666b449
Wachira-Dickson <dicksonwachira886@gmail.com>
2026-06-22 20:53:12 +0300

    Create a table and view the records

--- website/views.py
+++ website/views.py
@@ -3,8 +3,11 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
+
     #Check to see if it is loggin in
     if request.method == 'POST':
         username = request.POST['username']
@@ -16,7 +19,7 @@ def home(request):
             return render(request, 'home.html', {})
         else:
             messages.error(request, 'Invalid username or password. Please try again.')
-    return render(request, 'home.html', {})
+    return render(request, 'home.html', {'records':records})
 
 
 def logout_user(request):
@@ -41,3 +44,12 @@ def register_user(request):
         return render(request, 'register.html', {'form': form})
 
     return render(request, 'register.html', {'form': form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        #Lookup records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, "You must be logged in to see this page...")
+        return redirect('home')
